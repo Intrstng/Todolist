06e125534f056ec1b26fd9ab6ab278d4fdf699cd
Intrstng <intrstg@gmail.com>
2026-06-15 21:49:06 +0300

    tests: refactor tests to vitest

--- src/features/Todolists/model/__tests__/todoListsSlice.test.ts
+++ src/features/Todolists/model/__tests__/todoListsSlice.test.ts
@@ -1,6 +1,7 @@
+import { describe, test, expect, beforeEach } from 'vitest'
 import { todoListsActions, FilterValuesType, TodolistDomainType, todoListsReducer } from '../slices';
-import { v1 } from 'uuid';
-import { Status } from '../../../../app/slices/appSlice';
+import { v4 } from 'uuid';
+import { Status } from '@/app/slices/appSlice';
 
 let todolistID_1: string;
 let todolistID_2: string;
@@ -12,17 +13,17 @@ let newFilter_2: FilterValuesType;
 let entityStatus_1: Status;
 let entityStatus_2: Status;
 
-// We can use tests without beforeEach() because we work with PURE functions
-beforeEach(() => {
-  todolistID_1 = v1();
-  todolistID_2 = v1();
-  todolistTitle = 'New todolist`s name';
-  newFilter_1 = 'active';
-  newFilter_2 = 'completed';
-  entityStatus = 'idle';
-  entityStatus_1 = 'loading';
-  entityStatus_2 = 'succeeded';
-  state = [
+describe('todoLists reducer', () => {
+  beforeEach(() => {
+    todolistID_1 = v4();
+    todolistID_2 = v4();
+    todolistTitle = 'New todolist`s name';
+    newFilter_1 = 'active';
+    newFilter_2 = 'completed';
+    entityStatus = 'idle';
+    entityStatus_1 = 'loading';
+    entityStatus_2 = 'succeeded';
+    state = [
       {
         id: todolistID_1,
         title: 'Main tasks',
@@ -39,125 +40,101 @@ beforeEach(() => {
         addedDate: new Date(),
         order: 0,
       },
-    ]
-});
+    ];
+  });
 
-// ------------------- 'CHANGE-FILTER' ------------------- //
+  test('reducer todoLists should CHANGE FILTER', () => {
+    const newState_1 = todoListsReducer(
+        state,
+        todoListsActions.changeFilter({ todolistID: todolistID_1, value: newFilter_1 }),
+    );
+    const newState_2 = todoListsReducer(
+        state,
+        todoListsActions.changeFilter({ todolistID: todolistID_2, value: newFilter_2 }),
+    );
 
-test('reducer todoLists should CHANGE FILTER', () => {
-  // action
-  const newState_1 = todoListsReducer(
-    state,
-    todoListsActions.changeFilter({ todolistID: todolistID_1, value: newFilter_1 }),
-  );
-  const newState_2 = todoListsReducer(
-    state,
-    todoListsActions.changeFilter({ todolistID: todolistID_2, value: newFilter_2 }),
-  );
+    expect(state.filter((tl) => tl.id === todolistID_1)[0].filter).toBe('all');
+    expect(newState_1.filter((tl) => tl.id === todolistID_1)[0].filter).toBe(newFilter_1);
 
-  // expectation
-  expect(state.filter((tl) => tl.id === todolistID_1)[0].filter).toBe('all');
-  expect(newState_1.filter((tl) => tl.id === todolistID_1)[0].filter).toBe(newFilter_1);
+    expect(state.filter((tl) => tl.id === todolistID_2)[0].filter).toBe('active');
+    expect(newState_2.filter((tl) => tl.id === todolistID_2)[0].filter).toBe(newFilter_2);
+  });
 
-  expect(state.filter((tl) => tl.id === todolistID_2)[0].filter).toBe('active');
-  expect(newState_2.filter((tl) => tl.id === todolistID_2)[0].filter).toBe(newFilter_2);
-});
+  test('reducer todoLists should ADD-TODOLIST', () => {
+    const newTodoList = {
+      id: todolistID_1,
+      title: 'newTodoList',
+      filter: 'all',
+      addedDate: new Date(),
+      order: 0,
+    };
+    const newState = todoListsReducer(state, todoListsActions.addTodolist({ newTodolistData: newTodoList }));
 
-// ------------------- 'ADD-TODOLIST' ------------------- //
+    expect(state.length).toBe(2);
+    expect(newState.length).toBe(3);
+    expect(newState[2].id).toBeDefined();
+    expect(newState[2].title).toBe(newTodoList.title);
+    expect(newState[2].filter).toBe('all');
+  });
 
-test('reducer todoLists should ADD-TODOLIST', () => {
-  // action
-  const newTodoList = {
-    id: todolistID_1,
-    title: 'newTodoList',
-    filter: 'all',
-    addedDate: new Date(),
-    order: 0,
-  };
-  const newState = todoListsReducer(state, todoListsActions.addTodolist({ newTodolistData: newTodoList }));
+  test('reducer todoLists should REMOVE-TODOLIST', () => {
+    const newState_1 = todoListsReducer(state, todoListsActions.removeTodolist({ todolistID: todolistID_1 }));
+    const newState_2 = todoListsReducer(state, todoListsActions.removeTodolist({ todolistID: todolistID_2 }));
 
-  // expectation
-  expect(state.length).toBe(2);
-  expect(newState.length).toBe(3);
-  expect(newState[2].id).toBeDefined(); // index 2 is used because the new todolist adds to the end of todoLists array
-  expect(newState[2].title).toBe(newTodoList.title);
-  expect(newState[2].filter).toBe('all');
-});
+    expect(state.length).toBe(2);
+    expect(newState_1.length).toBe(1);
+    expect(newState_2.length).toBe(1);
 
-// ------------------- 'REMOVE-TODOLIST' ------------------- //
+    expect(newState_1[0]).toEqual(state[1]);
+    expect(newState_2[0]).toEqual(state[0]);
+  });
 
-test('reducer todoLists should REMOVE-TODOLIST', () => {
-  // action
-  const newState_1 = todoListsReducer(state, todoListsActions.removeTodolist({ todolistID: todolistID_1 }));
-  const newState_2 = todoListsReducer(state, todoListsActions.removeTodolist({ todolistID: todolistID_2 }));
+  test('reducer todoLists should UPDATE-TODOLIST (change todoLists title)', () => {
+    const newState_1 = todoListsReducer(
+        state,
+        todoListsActions.updateTodolist({ todolistID: todolistID_1, newTitle: todolistTitle }),
+    );
+    const newState_2 = todoListsReducer(
+        state,
+        todoListsActions.updateTodolist({ todolistID: todolistID_2, newTitle: todolistTitle }),
+    );
 
-  // expectation
-  expect(state.length).toBe(2);
-  expect(newState_1.length).toBe(1);
-  expect(newState_2.length).toBe(1);
+    expect(state[0].title).toBe('Main tasks');
+    expect(state[1].title).toBe('Prepare to the exam');
 
-  expect(newState_1[0]).toEqual(state[1]);
-  expect(newState_2[0]).toEqual(state[0]);
-});
+    expect(newState_1[0].title).toBe(todolistTitle);
+    expect(newState_2[1].title).toBe(todolistTitle);
+  });
 
-// ------------------- 'UPDATE-TODOLIST' ------------------- //
+  test('reducer todoLists should SET-TODOLISTS to the state (action creator for REST API request)', () => {
+    const newState = todoListsReducer([], todoListsActions.setTodoLists({ todolists: state }));
 
-test('reducer todoLists should UPDATE-TODOLIST (change todoLists title)', () => {
-  // action
-  const newState_1 = todoListsReducer(
-    state,
-    todoListsActions.updateTodolist({ todolistID: todolistID_1, newTitle: todolistTitle }),
-  );
-  const newState_2 = todoListsReducer(
-    state,
-    todoListsActions.updateTodolist({ todolistID: todolistID_2, newTitle: todolistTitle }),
-  );
+    expect(newState.length).toBe(2);
+    expect(newState[0].title).toBe('Main tasks');
+    expect(newState[0].filter).toBe('all');
+    expect(newState[1].filter).toBe('all');
+  });
 
-  // expectation
-  expect(state[0].title).toBe('Main tasks');
-  expect(state[1].title).toBe('Prepare to the exam');
+  test('reducer todoLists should CHANGE-TODOLIST-ENTITY-STATUS', () => {
+    const newState_1 = todoListsReducer(
+        state,
+        todoListsActions.changeTodoListsEntityStatus({
+          todolistID: todolistID_1,
+          entityStatus: entityStatus_1,
+        }),
+    );
+    const newState_2 = todoListsReducer(
+        state,
+        todoListsActions.changeTodoListsEntityStatus({
+          todolistID: todolistID_2,
+          entityStatus: entityStatus_2,
+        }),
+    );
 
-  expect(newState_1[0].title).toBe(todolistTitle);
-  expect(newState_2[1].title).toBe(todolistTitle);
-});
+    expect(state[0].entityStatus).toBe(entityStatus);
+    expect(state[1].entityStatus).toBe(entityStatus);
 
-// ------------------- 'SET-TODOLISTS' ------------------- //
-
-test('reducer todoLists should SET-TODOLISTS to the state (action creator for REST API request)', () => {
-  // action
-  const newState = todoListsReducer([], todoListsActions.setTodoLists({ todolists: state }));
-
-  // expectation
-  expect(newState.length).toBe(2);
-  expect(newState[0].title).toBe('Main tasks');
-  expect(newState[0].filter).toBe('all');
-  expect(newState[1].filter).toBe('all');
-  // expect(newState).toEqual(state);
-});
-
-// ------------------- 'CHANGE-TODOLIST-ENTITY-STATUS' ------------------- //
-
-test('reducer todoLists should CHANGE-TODOLIST-ENTITY-STATUS', () => {
-  // action
-  const newState_1 = todoListsReducer(
-    state,
-    todoListsActions.changeTodoListsEntityStatus({
-      todolistID: todolistID_1,
-      entityStatus: entityStatus_1,
-    }),
-  );
-  const newState_2 = todoListsReducer(
-    state,
-    todoListsActions.changeTodoListsEntityStatus({
-      todolistID: todolistID_2,
-      entityStatus: entityStatus_2,
-    }),
-  );
-
-  // expectation
-  expect(state[0].entityStatus).toBe(entityStatus);
-  expect(state[1].entityStatus).toBe(entityStatus);
-
-  expect(newState_1[0].entityStatus).toBe(entityStatus_1);
-  expect(newState_2[1].entityStatus).toBe(entityStatus_2);
+    expect(newState_1[0].entityStatus).toBe(entityStatus_1);
+    expect(newState_2[1].entityStatus).toBe(entityStatus_2);
+  });
 });
--- src/features/Todolists/model/__tests__/todolists-tasks-slice.test.ts
+++ src/features/Todolists/model/__tests__/todolists-tasks-slice.test.ts
@@ -1,7 +1,8 @@
+import { describe, test, expect, beforeEach } from 'vitest'
 import { TasksType, TodolistDomainType, todoListsActions, tasksReducer, todoListsReducer } from '../slices';
-import { v1 } from 'uuid';
-import { TaskPriorities, TaskStatuses } from '../../../../api/task-api';
-import { Status } from '../../../../app/slices/appSlice';
+import { v4 } from 'uuid';
+import { Status } from '@/app/slices/appSlice';
+import { TaskPriorities, TaskStatuses } from "@/api/task-api.ts";
 
 let todolistID_1: string;
 let todolistID_2: string;
@@ -10,175 +11,173 @@ let startTodoListsState: TodolistDomainType[];
 let newTodoListTitle: string;
 let entityStatus: Status;
 
-// We can use tests without beforeEach() because we work with PURE functions
-beforeEach(() => {
-  todolistID_1 = v1();
-  todolistID_2 = v1();
-  entityStatus = 'idle';
+describe('tasks reducer integration with todoLists', () => {
+  beforeEach(() => {
+    todolistID_1 = v4();
+    todolistID_2 = v4();
+    entityStatus = 'idle';
 
-  startTasksState = {
-    tasks: {
-      [todolistID_1]: [
+    startTasksState = {
+      tasks: {
+        [todolistID_1]: [
+          {
+            id: v4(),
+            title: 'HTML&CSS',
+            status: TaskStatuses.New,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_1,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+          {
+            id: v4(),
+            title: 'JS',
+            status: TaskStatuses.Completed,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_1,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+          {
+            id: v4(),
+            title: 'ReactJS',
+            status: TaskStatuses.New,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_1,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+        ],
+        [todolistID_2]: [
+          {
+            id: v4(),
+            title: 'Age',
+            status: TaskStatuses.Completed,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_2,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+          {
+            id: v4(),
+            title: 'Weight',
+            status: TaskStatuses.Completed,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_2,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+          {
+            id: v4(),
+            title: 'Height',
+            status: TaskStatuses.New,
+            description: '',
+            priority: TaskPriorities.Low,
+            startDate: new Date(),
+            deadline: new Date(),
+            todoListId: todolistID_2,
+            order: 0,
+            addedDate: new Date(),
+            entityStatus,
+          },
+        ],
+      }
+    };
+
+    startTodoListsState = [
+      {
+        id: todolistID_1,
+        title: 'Main tasks',
+        filter: 'all',
+        entityStatus,
+        addedDate: new Date(),
+        order: 0,
+      },
+      {
+        id: todolistID_2,
+        title: 'Prepare to the exam',
+        filter: 'active',
+        entityStatus,
+        addedDate: new Date(),
+        order: 0,
+      },
+    ];
+    newTodoListTitle = 'New TODO\`s title';
+      });
+
+    test('reducer taskList should ADD-NEW-TASKS-LIST (new empty array for tasks should be added when new todolist is added)', () => {
+      const newTodoList = {
+        id: todolistID_1,
+        title: 'newTodoList',
+        filter: 'all',
+        addedDate: new Date(),
+        order: 0,
+      };
+      const action = todoListsActions.addTodolist({ newTodolistData: newTodoList });
+      const endTasksState = tasksReducer(startTasksState, action);
+      const endTodoListsState = todoListsReducer(startTodoListsState, action);
+      const keys = Object.keys(endTasksState.tasks);
+      const idFromTasks = keys[0];
+      const idFromTodoLists = endTodoListsState[endTodoListsState.length - 1].id;
+
+      expect(idFromTasks).toBe(action.payload.newTodolistData.id);
+      expect(idFromTodoLists).toBe(action.payload.newTodolistData.id);
+    });
+
+    test('reducer taskList should DELETE-TASKS-LIST (delete array of tasks with ID of deleted todoList)', () => {
+      const action = todoListsActions.removeTodolist({ todolistID: todolistID_2 });
+      const endState = tasksReducer(startTasksState, action);
+      const keys = Object.keys(endState.tasks);
+
+      expect(keys.length).toBe(1);
+      expect(endState.tasks[todolistID_2]).not.toBeDefined();
+    });
+
+    test('reducer taskList should ADD-TASKS-LIST WHEN TODOLISTS-ADDED (when todolists loaded from Rest API)', () => {
+      const state = [
         {
-          id: v1(),
-          title: 'HTML&CSS',
-          status: TaskStatuses.New,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_1,
-          order: 0,
+          id: todolistID_1,
+          title: 'Main tasks',
+          filter: 'all',
           addedDate: new Date(),
-          entityStatus,
+          order: 0,
         },
         {
-          id: v1(),
-          title: 'JS',
-          status: TaskStatuses.Completed,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_1,
-          order: 0,
+          id: todolistID_2,
+          title: 'Prepare to the exam',
+          filter: 'active',
           addedDate: new Date(),
-          entityStatus,
-        },
-        {
-          id: v1(),
-          title: 'ReactJS',
-          status: TaskStatuses.New,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_1,
           order: 0,
-          addedDate: new Date(),
-          entityStatus,
         },
-      ],
-      [todolistID_2]: [
-        {
-          id: v1(),
-          title: 'Age',
-          status: TaskStatuses.Completed,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_2,
-          order: 0,
-          addedDate: new Date(),
-          entityStatus,
-        },
-        {
-          id: v1(),
-          title: 'Weight',
-          status: TaskStatuses.Completed,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_2,
-          order: 0,
-          addedDate: new Date(),
-          entityStatus,
-        },
-        {
-          id: v1(),
-          title: 'Height',
-          status: TaskStatuses.New,
-          description: '',
-          priority: TaskPriorities.Low,
-          startDate: new Date(),
-          deadline: new Date(),
-          todoListId: todolistID_2,
-          order: 0,
-          addedDate: new Date(),
-          entityStatus,
-        },
-      ],
-    }
-  };
-  startTodoListsState = [
-    {
-      id: todolistID_1,
-      title: 'Main tasks',
-      filter: 'all',
-      entityStatus,
-      addedDate: new Date(),
-      order: 0,
-    },
-    {
-      id: todolistID_2,
-      title: 'Prepare to the exam',
-      filter: 'active',
-      entityStatus,
-      addedDate: new Date(),
-      order: 0,
-    },
-  ];
-  newTodoListTitle = 'New TODO`s title';
-});
+      ];
 
-// ------------------- 'ADD-NEW-TASKS-LIST' ------------------- //
+      const action = todoListsActions.setTodoLists({ todolists: state });
+      const endState = tasksReducer({ tasks: {} }, action);
 
-test('reducer taskList should ADD-NEW-TASKS-LIST (new empty array for tasks should be added when new todolist is added)', () => {
-  const newTodoList = {
-    id: todolistID_1,
-    title: 'newTodoList',
-    filter: 'all',
-    addedDate: new Date(),
-    order: 0,
-  };
-  const action = todoListsActions.addTodolist({ newTodolistData: newTodoList });
-  const endTasksState = tasksReducer(startTasksState, action);
-  const endTodoListsState = todoListsReducer(startTodoListsState, action);
-  const keys = Object.keys(endTasksState);
-  const idFromTasks = keys[0];
-  const idFromTodoLists = endTodoListsState[endTodoListsState.length - 1].id;
-  expect(idFromTasks).toBe(action.payload.newTodolistData.id);
-  expect(idFromTodoLists).toBe(action.payload.newTodolistData.id);
-});
+      const keys = Object.keys(endState.tasks);
 
-// ------------------- 'DELETE-TASKS-LIST' ------------------- //
-
-test('reducer taskList should DELETE-TASKS-LIST (delete array of tasks with ID of deleted todoList)', () => {
-  const action = todoListsActions.removeTodolist({ todolistID: todolistID_2 });
-  const endState = tasksReducer(startTasksState, action);
-  const keys = Object.keys(endState);
-  expect(keys.length).toBe(1);
-  expect(endState.tasks[todolistID_2]).not.toBeDefined();
-});
-
-// ---------- 'ADD-TASKS-LIST WHEN TODOLISTS-ADDED' ---------- //
-
-test('reducer taskList should ADD-TASKS-LIST WHEN TODOLISTS-ADDED (when todolists loaded from Rest API)', () => {
-  let state = [
-    {
-      id: todolistID_1,
-      title: 'Main tasks',
-      filter: 'all',
-      addedDate: new Date(),
-      order: 0,
-    },
-    {
-      id: todolistID_2,
-      title: 'Prepare to the exam',
-      filter: 'active',
-      addedDate: new Date(),
-      order: 0,
-    },
-  ];
-
-  const action = todoListsActions.setTodoLists({ todolists: state });
-  const endState = tasksReducer({tasks: {}}, action);
-
-  const keys = Object.keys(endState);
-
-  expect(keys.length).toBe(2);
-  expect(endState.tasks[todolistID_1]).toStrictEqual([]);
-  expect(endState.tasks[todolistID_2]).toStrictEqual([]);
-});
+      expect(keys.length).toBe(2);
+      expect(endState.tasks[todolistID_1]).toStrictEqual([]);
+      expect(endState.tasks[todolistID_2]).toStrictEqual([]);
+    });
+  });
