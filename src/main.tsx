26c1dd30f3e742806e346e8168841c39fade0a08
Intrstng <intrstg@gmail.com>
2026-06-26 20:27:46 +0300

    fix: add basename to BrowserRouter for correct deploy to github-pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "@/app/App.tsx";
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/Todolist">
         <Provider store={store}>
             <App/>
         </Provider>
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/Todolist', // for deploy to github-pages - not used for vercel deploy
+  base: '/Todolist/', // for deploy to github-pages - not used for vercel deploy
   plugins: [react()],
   resolve: {
     alias: {
